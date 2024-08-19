0f80f5cb130d2ab51a792d24f58651a2e02504db
YasseenR <yasseenrouni@gmail.com>
2024-08-19 18:20:19 -0400

    Utilized static function inside struct to sort the struct of users when .sorted() is called on it.

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+
+struct User: Comparable, Identifiable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+    
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
 struct ContentView: View {
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister")
+    ].sorted()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(users) { user in
+            Text("\(user.lastName), \(user.firstName)")
         }
-        .padding()
     }
 }
 
