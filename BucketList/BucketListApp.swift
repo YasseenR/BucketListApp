19410e3e0666096ee6d1f8fb6f077ce0139826d8
YasseenR <yasseenrouni@gmail.com>
2024-08-19 18:02:17 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Yasseen Rouni on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
